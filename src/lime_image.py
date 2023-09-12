82f86a5299ab91a21d6353842054c0ec0c128dab
M4yfl0w3r <mr.mayflower15@gmail.com>
2023-09-12 22:17:50 +0200

    * add random numbers from Bernoulli distribution

--- src/lime_image.py
+++ src/lime_image.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np 
 import matplotlib.pyplot as pyplot
 
+from scipy.stats import bernoulli
 from skimage.segmentation import quickshift
 from skimage.segmentation import mark_boundaries 
 
@@ -16,8 +17,11 @@ class ImageExplainer:
         """Function that generates a new dataset that 
            will be used to train a simple linear model."""
         
-        superpixels: np.ndarray = quickshift(self.image, kernel_size = 4)
+        superpixels: np.ndarray = quickshift(self.image, kernel_size = 5, ratio = 0.5)
         boundaries: np.ndarray  = mark_boundaries(self.image, superpixels)
+
+        num_superpixels = superpixels.max() + 1
+        interpretable_features = bernoulli.rvs(p = 0.5, size = num_superpixels)
 
         pyplot.imshow(boundaries)
         pyplot.axis('off')
