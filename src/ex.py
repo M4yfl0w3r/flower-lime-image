a696fc02fe00d82f68dcaa6f4e3e48903ac8bd2f
M4yfl0w3r <mr.mayflower15@gmail.com>
2023-09-19 20:52:36 +0200

    * add calculate weights function

--- src/ex.py
+++ src/ex.py
@@ -17,26 +17,25 @@ image = Image.open(test_image_path)
 
 model_path = Path('../assets/model.pth')
 model = CNN()
-model.load_state_dict(torch.load(str(model_path), map_location = torch.device('cpu')))
+model.load_state_dict(torch.load(str(model_path), map_location=torch.device('cpu')))
 
 @torch.no_grad()
 def batch_predict(image: np.ndarray) -> int:
     transform = transforms.Compose([
-        transforms.Resize((223, 224)),
+        transforms.Resize((224, 224)),
         transforms.ToTensor()
     ])
 
     image: Image = Image.fromarray(image)
     image: torch.Tensor = transform(image).float()
     image: torch.Tensor = image.to(device)
-    image: torch.Tensor = image.unsqueeze(0) 
+    image: torch.Tensor = image.unsqueeze(0)
 
     model.eval()
     output: torch.Tensor = model(image)
     return output.data.cpu().numpy().argmax()
 
-explainer = ImageExplainer(np.array(image), batch_predict)
-dataset   = explainer.generate_dataset(num_samples = 1)
 
-for element in dataset:
-    element.draw()
+explainer = ImageExplainer(np.array(image), batch_predict)
+dataset = explainer.generate_dataset(num_samples=1)
+weights = explainer.calculate_weights()
--- src/lime_image.py
+++ src/lime_image.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as pyplot
 
 from typing import Callable
 from scipy.stats import bernoulli
+from sklearn.metrics import pairwise_distances
 from skimage.segmentation import quickshift
 from skimage.segmentation import mark_boundaries 
 
@@ -13,7 +14,7 @@ class DatasetElement:
         self.image = image 
         self.label = label
 
-    def draw(self):
+    def draw(self) -> None:
         pyplot.imshow(self.image)
         pyplot.title(f"Label = {self.label}")
         pyplot.axis('off')
@@ -25,14 +26,14 @@ class ImageExplainer:
     def __init__(self, image: np.ndarray, classifier_fn: Callable[[np.ndarray], int]):
         self.image = image 
         self.classifier_fn = classifier_fn
+        self.generated_dataset: list[DatasetElement] = []
 
-    def generate_dataset(self, num_samples: int):
-        """Function that generates a new dataset that will be used to train a simple linear model."""
+    def generate_dataset(self, num_samples: int) -> list[DatasetElement]:
+        """Function that generates a new dataset that will be used to train the simple linear model."""
         
         superpixels: np.ndarray = quickshift(self.image, kernel_size = 5, ratio = 0.5)
         num_superpixels: int    = superpixels.max() + 1
-        replacement_color: int  = 170 
-        generated_dataset: list[DatasetElement] = []
+        replacement_color: int  = 170
 
         for _ in range(num_samples):
             interpretable_features: np.ndarray = bernoulli.rvs(p = 0.5, size = num_superpixels)
@@ -44,9 +45,23 @@ class ImageExplainer:
                     image_with_masked_superpixels[superpixel_mask] = replacement_color
 
             prediction = self.classifier_fn(image_with_masked_superpixels)
-            generated_dataset.append(DatasetElement(image_with_masked_superpixels, prediction))
+            self.generated_dataset.append(DatasetElement(image_with_masked_superpixels, prediction))
 
-        return generated_dataset
+        return self.generated_dataset
+    
+    def calculate_weights(self) -> list[int]:
+        """Function that calculates the simple linear model weights based on differences between original
+           and generated images."""
+        weights: list[float] = []
+
+        for element in self.generated_dataset:
+            generated_image: np.ndarray = element.image
+            cosine_distance: np.ndarray = pairwise_distances(self.image.ravel().reshape(1, -1), 
+                                                             generated_image.ravel().reshape(1, -1), 
+                                                             metric = 'cosine')
+            weights.append(cosine_distance)
+
+        return weights
     
     def show_superpixels_boundaries(self, superpixels: np.ndarray) -> None:
         boundaries: np.ndarray = mark_boundaries(self.image, superpixels)
